4046552e4de119a7b77907167d1f3035e83e78c3
aallm004 <abigailrosea10@gmail.com>
2024-07-11 17:47:15 -0500

    .

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -2,7 +2,7 @@
 """
 Python script that returns info about his/her TODO list progress
 """
-import requests
+import urllib.request
 import sys
 import csv
 import json
@@ -14,9 +14,11 @@ def get_employee_todo_progress(employee_id):
     employee_url = f"{base_url}/users/{employee_id}"
     todo_url = f"{base_url}/todos?userId={employee_id}"
 
-    employee_info = requests.get(employee_url).json()
+    with urllib.request.urlopen(employee_url) as response:
+            employee_info = response.read().json()
+    with urllib.request.urlopen(employee_url) as response:
+            todo_list = response.read().json()
     employee_name = employee_info['username']
-    todo_list = requests.get(todo_url, params={"userId": employee_id}).json()
 
     completed_todo = [x["title"] for x in todo_list if x["completed"]]
     total_todo = len(todo_list)
