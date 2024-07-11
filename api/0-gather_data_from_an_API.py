7051e52b4c2c2cc589c81fba79f6daba5a98dfa0
aallm004 <abigailrosea10@gmail.com>
2024-07-11 13:44:42 -0500

    0?

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -7,32 +7,25 @@ import sys
 
 
 def get_employee_todo_progress(employee_id):
-    api_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
-    todo_url = f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
     
-    try:
-        user_response = requests.get(api_url)
-        todos_response = requests.get(todo_url)
-        
-        user_data = user_response.json()
-        todos_data = todos_response.json()
+    api_url = "https://jsonplaceholder.typicode.com/"
+    employee_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    todo_url = f"https://jsonplaceholder.typicode.com/todos"
 
-        employee_name = user_data.get("name")
-        total_tasks = len(todos_data)
-        done_tasks = sum(1 for todo in todos_data.get if todo.get("completed"))
 
-        print(f"Employee: {employee_name} is done with tasks \
-              ({done_tasks}/{total_tasks}):")
-        print(f"{employee_name}: {done_tasks} completed tasks")
-        print(f"Total tasks: {total_tasks}")
+    employee_info = requests.get("employ_url").json()
+    employee_name = employee_info.get("name")
+    todo_list = requests.get(todo_url, p={"userId": employee_id}).json()
 
-        for todo in todos_data:
-            if todo.get("completed"):
-                print(f"{todo.get('title')}")
+    completed_todo = [x["title"] for x in todo_list if x["completed"]]
+    total_todo = len(todo_list)
+    total_complete = len(completed_todo)
 
-    except requests.RequestException as e:
-        print(f"Error fetching data: {e}")
+    print("Employee {} is done with tasks({}/{}):"
+          .format(employee_name, total_complete, total_todo))
 
-    if len(sys.argv) > 1:
-        print("Incorrect, not <employee_id>")
-        sys.exit(1)
+    for todo in completed_todo:
+        print("\t {}".format(todo))
+
+if __name__ == "__main__":
+    get_employee_todo_progress(int(sys.argv[1]))
