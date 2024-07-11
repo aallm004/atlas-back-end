fb004d1cfafca6650ba678244039dbc173728c41
aallm004 <abigailrosea10@gmail.com>
2024-07-11 17:33:42 -0500

    f.write(x)

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -32,7 +32,7 @@ def get_employee_todo_progress(employee_id):
                 f'"{completed_todo}",' + \
                 f'"{todo_list}"'
 
-            f.write(out)
+            f.write(x)
 
 
 if __name__ == "__main__":
