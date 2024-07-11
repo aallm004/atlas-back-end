b88b0275535fccb7e819f57aa420bb932e8a7b87
aallm004 <abigailrosea10@gmail.com>
2024-07-11 13:19:36 -0500

    .

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -7,25 +7,28 @@ import sys
 
 
 def get_employee_todo_progress(employee_id):
-    api_url = f"https://jsonplaceholder.typicode.com/"
-
+    api_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    todo_url = f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
+    
     try:
-        response = requests.get(api_url)
-        response_data = response.json()
+        user_response = requests.get(api_url)
+        todos_response = requests.get(todo_url)
+        
+        user_data = user_response.json()
+        todos_data = todos_response.json()
 
-        employee_name = response_data.get("employee_name")
-        total_tasks = len(response_data.get("todos"))
-        done_tasks = sum(1 for todo in response_data.get("todos") if todo.get
-                         ("status") == "Done")
+        employee_name = user_data.get("name")
+        total_tasks = len(todos_data)
+        done_tasks = sum(1 for todo in todos_data.get if todo.get("completed")
 
         print(f"Employee: {employee_name} is done with tasks ({done_tasks} \
               {total_tasks}):")
         print(f"{employee_name}: {done_tasks} completed tasks")
         print(f"Total tasks: {total_tasks}")
 
-        for todo in response_data.get("todos"):
-            if todo.get("status") == "Done":
-                print(f"\t{todo.get('title')}")
+        for todo in todos_data:
+            if todo.get("completed"):
+                print(f"\t{todo.get("title")}")
 
     except requests.RequestException as e:
         print(f"Error fetching data: {e}")
@@ -33,6 +36,3 @@ def get_employee_todo_progress(employee_id):
     if len(sys.argv) > 1:
         print("Incorrect, not <employee_id>")
         sys.exit(1)
-
-    employee_id = int(sys.argv[1])
-    get_todo_progress(employee_id)
