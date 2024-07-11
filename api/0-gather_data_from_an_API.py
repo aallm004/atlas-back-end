fa78bc83c1ea2baa51890ee0fc5ca03fbf16bdb3
aallm004 <abigailrosea10@gmail.com>
2024-07-11 14:11:42 -0500

    zer0 again

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -12,7 +12,7 @@ def get_employee_todo_progress(employee_id):
     employee_url = f"{base_url}/users/{employee_id}"
     todo_url = f"{base_url}/todos?userId={employee_id}"
 
-    employee_info = requests.get("employee_url").json()
+    employee_info = requests.get(employee_url).json()
     employee_name = employee_info['name']
     todo_list = requests.get(todo_url, p={"userId": employee_id}).json()
 
