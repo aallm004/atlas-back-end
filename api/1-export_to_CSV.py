380c27abf072b5d08b37c9dd7d7035996aa882d7
aallm004 <abigailrosea10@gmail.com>
2024-07-12 17:24:30 -0500

    1

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -2,29 +2,42 @@
 """
 Python script that returns info about his/her TODO list progress
 """
-import requests
+import json
 import sys
+import urllib.request
 
 
 def get_employee_todo_progress(employee_id):
-    
+
     base_url = "https://jsonplaceholder.typicode.com/"
     employee_url = f"{base_url}/users/{employee_id}"
     todo_url = f"{base_url}/todos?userId={employee_id}"
-    
-    employee_info = requests.get(employee_url).json()
-    employee_name = employee_info['username']
-    todo_list = requests.get(todo_url, params={"userId": employee_id}).json()
-    
+
+    with urllib.request.urlopen(employee_url) as response:
+        employee_info = response.read()
+    employee_info = json.loads(employee_info)
+
+    with urllib.request.urlopen(todo_url) as response:
+        todo_list = response.read()
+    todo_list = json.loads(todo_list)
+
+    employee_name = employee_info['name']
+
     completed_todo = [x["title"] for x in todo_list if x["completed"]]
     total_todo = len(todo_list)
     total_complete = len(completed_todo)
-    
+
     print("Employee {} is done with tasks({}/{}):"
           .format(employee_name, total_complete, total_todo))
 
-    for todo in completed_todo:
-            print(f"\t{todo}")
+    with open(f'{employee_id}.csv', 'w') as f:
+        for todo in todo_list:
+            x = f'"{employee_id}",' + \
+                f'"{employee_name}",' + \
+                f'"{completed_todo}",' + \
+                f'"{todo_list}"'
+            f.write(x)
 
-    if __name__ == "__main__":
-        get_employee_todo_progress(int(sys.argv[1]))
+
+if __name__ == "__main__":
+    get_employee_todo_progress(int(sys.argv[1]))
