c1091b256326cd2a41b10f71987057efb1a60f54
aallm004 <abigailrosea10@gmail.com>
2024-07-12 17:19:07 -0500

    0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -4,6 +4,7 @@ Python script that returns info about his/her TODO list progress
 """
 import sys
 import urllib.request
+import json
 
 
 def get_employee_todo_progress(employee_id):
@@ -13,10 +14,14 @@ def get_employee_todo_progress(employee_id):
     todo_url = f"{base_url}/todos?userId={employee_id}"
 
     with urllib.request.urlopen(employee_url) as response:
-        employee_info = response.read().json()
-    employee_name = employee_info['username']
+        employee_info = response.read()
+    employee_info = json.loads(employee_info)
+
     with urllib.request.urlopen(todo_url) as response:
-        todo_list = response.read().json()
+        todo_list = response.read()
+    todo_list = json.loads(todo_list)
+
+    employee_name = employee_info['name']
 
     completed_todo = [x["title"] for x in todo_list if x["completed"]]
     total_todo = len(todo_list)
@@ -28,5 +33,5 @@ def get_employee_todo_progress(employee_id):
     for todo in completed_todo:
         print(f"\t {todo}")
 
-    if __name__ == "__main__":
-        get_employee_todo_progress(int(sys.argv[1]))
+if __name__ == "__main__":
+    get_employee_todo_progress(int(sys.argv[1]))
