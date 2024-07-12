2ade0bfa625d85fd45b4a3d3e220b029a2a5e20f
aallm004 <abigailrosea10@gmail.com>
2024-07-12 12:29:31 -0500

    1

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -15,7 +15,7 @@ def get_employee_todo_progress(employee_id):
     todo_url = f"{base_url}/todos?userId={employee_id}"
 
     with urllib.request.urlopen(employee_url) as response:
-        employee_info = response.read().json()
+        employee_info = json.loads(response.read().decode())
     with urllib.request.urlopen(employee_url) as response:
         todo_list = response.read().json()
     employee_name = employee_info['username']
@@ -31,9 +31,12 @@ def get_employee_todo_progress(employee_id):
     with open(f'{employee_id}.csv', 'w', newline='') as f:
         writer = csv.writer(f, quoting=csv.QUOTE_ALL)
         for todo in todo_list:
-            writer.writerow([employee_id, employee_name,
+            writer.writerow([employee_id, employee_name, \
                              completed_todo, todo_list])
 
 
-if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <employee_id>")
+        sys.exit(1)
     get_employee_todo_progress(int(sys.argv[1]))
+
