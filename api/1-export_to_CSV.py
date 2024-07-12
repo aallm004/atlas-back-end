30003a5491aa3f2974958b5cd7f7ae5083982fe4
aallm004 <abigailrosea10@gmail.com>
2024-07-12 12:18:19 -0500

    1

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -27,15 +27,14 @@ def get_employee_todo_progress(employee_id):
     print("Employee {} is done with tasks({}/{}):"
           .format(employee_name, total_complete, total_todo))
 
-    for todo in todo_list:
-        x = f'"{employee_id}",' + \
-            f'"{employee_name}",' + \
-            f'"{completed_todo}",' + \
-            f'"{todo_list}"'
-
     csv_file_name = f"{employee_id}.csv"
-    with open(f'{employee_id}.csv', 'w') as f:
-        f.write(x)
+    with open(f'{employee_id}.csv', 'w', newline='') as f:
+        write = csv.writer(f, quoting=csv.QUOTE_ALL)
+        for todo in todo_list:
+            write.writerow(f'"{employee_id}",' + \
+                           f'"{employee_name}",' + \
+                           f'"{completed_todo}",' + \
+                           f'"{todo_list}"')
 
 
 if __name__ == "__main__":
